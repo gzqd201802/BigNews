7af87c0606518ae9328089beb695467150bc85b8
SuSu <sujiehao@itcast.cn>
2019-12-28 17:56:06 +0800

    urls用对象统一封装处理

--- admin/js/http.js
+++ admin/js/http.js
@@ -17,4 +17,23 @@
             }
         }
     });
+
+    // 完整的URL参考: http://localhost:8080/api/v1/admin/user/info
+
+    // 设置请求的服务器根路径，用于后续完整 url 的拼接
+    const baseURL = 'http://localhost:8080/api/v1';
+    // 用一个 urls 对象管理项目所有的请求地址
+    const urls = {
+        // 2、获取用户信息
+        userInfo: baseURL + '/admin/user/info',
+        // 3、获取用户详情
+        userDetail: baseURL + '/admin/user/detail',
+        //  4、编辑用户信息
+        userDetail: baseURL + '/admin/user/edit',
+        // 后续其他的请求地址自己拼接完善
+    };
+
+    // 把 urls 对象暴露到全局 window 对象中
+    window.urls = urls;
+
 })(window);

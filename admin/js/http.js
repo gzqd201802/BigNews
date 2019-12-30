3a611888710ecf393539fa89ee150cfe1b390140
SuSu <sujiehao@itcast.cn>
2019-12-30 10:06:11 +0800

    获取用户数据并渲染

--- admin/js/http.js
+++ admin/js/http.js
@@ -24,13 +24,25 @@
     const baseURL = 'http://localhost:8080/api/v1';
     // 用一个 urls 对象管理项目所有的请求地址
     const urls = {
-        // 2、获取用户信息
-        userInfo: baseURL + '/admin/user/info',
-        // 3、获取用户详情
-        userDetail: baseURL + '/admin/user/detail',
-        //  4、编辑用户信息
-        userDetail: baseURL + '/admin/user/edit',
-        // 后续其他的请求地址自己拼接完善
+        baseURL:baseURL,//基地址
+        user_login:      baseURL + '/admin/user/login',//用户登录
+        user_info:       baseURL + '/admin/user/info',//用户信息
+        user_detail:     baseURL + '/admin/user/detail',//用户详情
+        user_edit:       baseURL + '/admin/user/edit',//用户编辑
+        category_list:   baseURL + '/admin/category/list',//文章类别查询
+        category_add:    baseURL + '/admin/category/add',//文章类别新增
+        category_search: baseURL + '/admin/category/search',//文章类别搜索
+        category_edit:   baseURL + '/admin/category/edit',//文章类别编辑
+        category_delete: baseURL + '/admin/category/delete',//文章类别删除
+        article_query:   baseURL + '/admin/article/query',//文章搜索
+        article_publish: baseURL + '/admin/article/publish',//文章发布
+        article_search:  baseURL + '/admin/article/search',//文章信息查询
+        article_edit:    baseURL + '/admin/article/edit',//文章编辑
+        article_delete:  baseURL + '/admin/article/delete',//文章删除
+        comment_list:    baseURL + '/admin/comment/search',//文章评论列表
+        comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
+        comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
+        comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
     };
 
     // 把 urls 对象暴露到全局 window 对象中
